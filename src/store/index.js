d5a5f83c3be6e60a55e655d85ebd80ccb80c1a98
Omar Khan <omakhan98@gmail.com>
2023-07-12 16:40:43 +0500

    added persistor

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage/session";
 import { useDispatch } from "react-redux";
 
 const initialState = {
